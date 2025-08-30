db37667bfe6648d08522292e3dcd004fc13271ed
abdullah107189 <abdullah107189@gmail.com>
2025-08-30 18:11:47 +0600

    fixed findNearByRides show

--- src/app/modules/ride/ride.model.ts
+++ src/app/modules/ride/ride.model.ts
@@ -61,5 +61,5 @@ const rideSchema = new Schema<IRide>(
   },
   { timestamps: true }
 );
-
+rideSchema.index({ "pickupLocation.location": "2dsphere" });
 export const Ride = model<IRide>("Ride", rideSchema);
