4c69d6a33b6be22bb055db478b1078b2f3a8aa6c
abdullah107189 <abdullah107189@gmail.com>
2025-10-26 22:31:25 +0600

    set origin

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://ride-booking-system-frontend-omega.vercel.app",
+    origin: "https://ride-booking-system-frontend-omega.vercel.app/",
     credentials: true,
   })
 );
